af2fe327bf2084ba5e7e1ecb2431e4f3c3a421b8
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-02-11 15:48:15 +0200

    Delete 02-inches-to-centimeters.js

--- 02.Simple-Calculations/02-inches-to-centimeters.js
+++
@@ -1,7 +0,0 @@
-function inchesToCentimeters([arg1]) {
-    let inches = parseFloat(arg1);
-    let centimeters = inches * 2.54;
-    console.log("centimeters = " + centimeters);
-}
-
-inchesToCentimeters([5]);
