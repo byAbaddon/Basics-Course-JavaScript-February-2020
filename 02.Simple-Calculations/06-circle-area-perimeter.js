4c8dacb201125d534074526a6ed22c52ac567616
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-02-11 15:56:37 +0200

    Delete 06-circle-area-perimeter.js

--- 02.Simple-Calculations/06-circle-area-perimeter.js
+++
@@ -1,8 +0,0 @@
-function circleAreaPerimeter([arg1]) {
-    let r = Number(arg1);
-    console.log(Math.PI * r * r);
-    console.log(2 * Math.PI * r);
-}
-
-circleAreaPerimeter([3]);
-circleAreaPerimeter([4.5]);
