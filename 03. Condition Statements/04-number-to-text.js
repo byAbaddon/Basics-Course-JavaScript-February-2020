e8c1d2c2774c46f42f4eb7b5b8b13e604aac6327
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-02-15 16:22:32 +0200

    Update 04-number-to-text.js

--- 03. Condition Statements/04-number-to-text.js
+++ 03. Condition Statements/04-number-to-text.js
@@ -1,20 +1,6 @@
 function numberToText(arg) {
-
-  switch (arg[0]) {
-    case '0' : r ='zero'; break;
-    case '1' : r ='one'; break;
-    case '2' : r ='two'; break;
-    case '3' : r ='three'; break;
-    case '4' : r ='four'; break;
-    case '5' : r ='five'; break;
-    case '6' : r ='six'; break;
-    case '7' : r ='seven'; break;
-    case '8' : r ='eight'; break;
-    case '9' : r ='nine'; break;
-     default: r = 'number too big'
-      break;
-  }
-  console.log(r); 
- }
+  arr  = ['zero','one','two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine', ]
+  console.log( arg[0] < 10 ? arr[arg[0]] : 'number too big');
+}
  
- //numberToText(['0'])
+//numberToText(['0'])
