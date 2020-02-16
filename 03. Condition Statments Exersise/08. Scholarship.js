1b8155147319d1a8989f08b360f62f9b2fea1749
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-02-16 21:59:07 +0200

    Update 08. Scholarship.js

--- 03. Condition Statments Exersise/08. Scholarship.js
+++ 03. Condition Statments Exersise/08. Scholarship.js
@@ -1,27 +1,19 @@
+//Bug №2 Zerro Unitest   
 function scholarship(arg) {
-  [income, grade, minSalary] = [arg[0], arg[1], arg[2]]
-  stipendia = 0 
-  excellent = 0
-
-  income < minSalary && grade > 4.50 ? stipendia = minSalary * 0.35 : null
-  grade > 5.50 ?  excellent = grade * 25 : null
-
- if (excellent > stipendia ) {
-     console.log (`You get a scholarship for excellent results ${Math.floor(excellent)} BGN`)
- }
- else if (excellent < stipendia && income < minSalary) {
-     console.log (`You get a Social scholarship ${Math.floor(stipendia)} BGN`)
- }
- else {
-     console.log ("You cannot get a scholarship!")
- }
-
+  [income, grade, minSalary] = [+arg[0], +arg[1], +arg[2]]
+     stipendia = minSalary * 0.35
+     excellentSitpemdia = grade * 25    
+  if (income < minSalary && grade > 4.5 && grade < 5.5 ) {
+      console.log(`You get a Social scholarship ${Math.floor(stipendia)} BGN`)
+  } else if (grade >= 5.5) {
+      console.log(`You get a scholarship for excellent results ${Math.floor(excellentSitpemdia)} BGN`)
+  } else {
+      console.log("You cannot get a scholarship!")
+  }
 }
-//87 points only
 
-// scholarship(['480.00','4.60','450.00'])
-// scholarship(['300.00','5.65','420.00'])
-// scholarship(['300.00','5.65','420.00'])
+// scholarship([ '480.00','4.60', '450.00',])  //141 BGN Not mast be 141
+// scholarship(['300.00','5.65', '420.00',])
 
 
 
