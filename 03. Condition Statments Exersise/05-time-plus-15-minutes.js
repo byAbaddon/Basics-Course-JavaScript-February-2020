d57a7bedf763bf0a9fc4e022b0f41fa7718b4655
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-02-16 22:28:04 +0200

    Update 05-time-plus-15-minutes.js

--- 03. Condition Statments Exersise/05-time-plus-15-minutes.js
+++ 03. Condition Statments Exersise/05-time-plus-15-minutes.js
@@ -1,6 +1,6 @@
-function timePlusMinutes(arg) {
-  let [start, end] = [0, 5]
-  time = new Date(0, 0, 0, arg[0], (+arg[1] + 15))
+function timeAddMinutes(arg) {
+  let [hour, min, start, end] = [+arg[0], +arg[1], 0, 5]
+  time = new Date(0, 0, 0, hour, min + 15)
   time.getHours() < 10 ? ++start && --end: null 
   console.log(time.toString().split(' ')[4].substr(start, end)); 
 }
