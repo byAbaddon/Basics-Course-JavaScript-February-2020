23babcb1dc9a6f5202b12fd5269ab75f256148a9
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-02-22 01:19:18 +0200

    Update 03-summerOutfit.js

--- 3. Nested Conditional Statments Exersise/03-summerOutfit.js
+++ 3. Nested Conditional Statments Exersise/03-summerOutfit.js
@@ -4,7 +4,7 @@ function summerOutfit(arg) {
                m: {Morning:'Sweatshirt and Sneakers', Afternoon: 'Shirt and Moccasins', Evening: 'Shirt and Moccasins'},      
                a: {Morning:'Shirt and Moccasins',     Afternoon: 'T-Shirt and Sandals', Evening: 'Shirt and Moccasins'},          
                e: {Morning:'T-Shirt and Sandals',     Afternoon: 'Swim Suit and Barefoot', Evening: 'Shirt and Moccasins'},   
-  }
+            }
 
   switch (true) {
     case degrees >= 10 && degrees <= 18:
@@ -38,7 +38,7 @@ function summerOutfit(arg) {
   console.log(`It's ${degrees} degrees, get your ${result}.`);
 }
 
-summerOutfit(['16', 'Morning'])
-summerOutfit(['16', 'Afternoon'])
-summerOutfit(['22', 'Afternoon'])
-summerOutfit(['28', 'Evening'])
+//summerOutfit(['16', 'Morning'])
+//summerOutfit(['16', 'Afternoon'])
+//summerOutfit(['22', 'Afternoon'])
+//summerOutfit(['28', 'Evening'])
