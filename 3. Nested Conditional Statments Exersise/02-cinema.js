a93ddd9eb3ece343d3391c7540956d355ffb31f6
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-02-22 01:17:14 +0200

    Update 02-cinema.js

--- 3. Nested Conditional Statments Exersise/02-cinema.js
+++ 3. Nested Conditional Statments Exersise/02-cinema.js
@@ -3,4 +3,5 @@ function cinema(arg) {
   let projectionType = { Premiere: 12.0, Normal: 7.5, Discount: 5.0 };
   console.log((projectionType[type] * row * col).toFixed(2) + " leva");
 }
-cinema(["Premiere", "10", "12"]);
+
+//cinema(["Premiere", "10", "12"]);
