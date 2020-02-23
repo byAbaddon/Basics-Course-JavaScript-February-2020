a31fc80ac7ecb951e4d6d9a7937c634cf8aa0904
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-02-23 02:13:42 +0200

    Add files via upload

---
+++ 4.0 Loops/10-oddEvenSum.js
@@ -0,0 +1,15 @@
+function oddEvenSum(arg) {
+  evenSum = 0
+  oddSum = 0
+
+  for (let i = 1; i < arg.length; i++) {
+    if ((i & 1) === 0) {
+      evenSum += +arg[i]
+    } else {
+      oddSum += +arg[i]
+    }
+  }
+  console.log(evenSum === oddSum ? `Yes\nSum = ${evenSum}` : `No\nDiff = ${Math.abs(evenSum - oddSum)}`);
+}
+// oddEvenSum(['4', '10', '50', '60', '20'])
+// oddEvenSum(['4', '3', '5', '1', '-2'])
---
+++ 4.0 Loops/11-cleverLily.js
@@ -0,0 +1,26 @@
+function cleverLily(arg) {
+  [age, laundryPrice, toysPrice] = [+arg[0], +arg[1], +arg[2]]
+   moneyCount = 0;
+   toysCount = 0;
+  let brotherReket = 0; 
+   for (let i = 1; i <= age; i++) {
+     if ((i & 1) === 0) {
+       moneyCount += (10 * i);
+       brotherReket++; 
+     } else {
+         toysCount++;
+       };
+   }; 
+     toysCount *= toysPrice ;
+  
+     let subtotal = (toysCount + (moneyCount / 2)) - brotherReket; // 550 + 33 -10 = 573
+     let total = Math.abs(subtotal - laundryPrice);
+
+     if (subtotal >= laundryPrice ) {
+        console.log(`Yes! ${(total.toFixed(2))}`);
+      
+     } else {
+      console.log(`No! ${(total.toFixed(2))}`);
+      };
+}
+cleverLily(['10', '170', '6']) 
