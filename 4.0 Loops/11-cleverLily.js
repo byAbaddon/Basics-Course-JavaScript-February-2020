e0c7c8b6b8857a8e8c2efe57a07b8dcc475ded68
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2020-11-01 22:49:05 +0200

    Update 11-cleverLily.js

--- 4.0 Loops/11-cleverLily.js
+++ 4.0 Loops/11-cleverLily.js
@@ -1,14 +1,8 @@
-function smartLily(...arg) {                                    //[be black ninja]//
-    let [age, washingMachinePrice, toyPrice] = [...arg]
-    let [toysCount, moneyFromBirthdays] = [0, 0]
-    for(let i = 1; i <= age; i ++){
-        i % 2 == 1 ? toysCount++ : (moneyFromBirthdays += 10 * i / 2, moneyFromBirthdays--)
-    }
-    
+function smartLily(...arg) {      //[be black ninja]//
+    let [age, washingMachinePrice, toyPrice, toysCount, moneyFromBirthdays] = [...arg, 0, 0]
+    for (let i = 1; i <= age; i++) i % 2 == 1 ? toysCount++ : (moneyFromBirthdays += 10 * i / 2, moneyFromBirthdays--)
     let money = moneyFromBirthdays + toyPrice * toysCount
-    return  money >= washingMachinePrice 
-    ? `Yes! ${(money - washingMachinePrice).toFixed(2)}`
-    : `No! ${(washingMachinePrice - money).toFixed(2)}`
+    return money >= washingMachinePrice? `Yes! ${(money - washingMachinePrice).toFixed(2)}`: `No! ${(washingMachinePrice - money).toFixed(2)}`
 }
 
 // console.log(smartLily(10, 170.00, 6))
